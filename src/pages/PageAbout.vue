901abb3d37b95eeb1ab22943f3970ba641230f41
Mu'adz Bin Mohd Rosli <muadz.mrosli@gmail.com>
2023-10-07 12:22:27 +0800

    feat: add lorem content to about page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,11 +1,35 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-h4">About Qwitter</h4>
+    <p class="text-body1">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae,
+      doloribus impedit! Expedita possimus animi sed deleniti cumque numquam nam
+      quisquam, labore quae, mollitia quis vitae doloremque cupiditate nemo aut
+      explicabo esse sequi autem unde? Explicabo voluptatum nulla ad ipsam,
+      nihil temporibus! Ducimus eum esse totam quaerat aspernatur quia eligendi,
+      debitis expedita consequuntur quod corrupti. Enim, deleniti aliquid, in
+      odio laborum similique illo ducimus praesentium rem adipisci excepturi
+      pariatur temporibus placeat iure iusto nam dolorum quia recusandae
+      voluptate debitis? Cum, corporis doloribus. Nostrum autem ipsa id sunt
+      officiis consequuntur, eveniet voluptas quibusdam commodi deserunt
+      provident error ullam eligendi minus. Officiis, ratione.
+    </p>
+    <p class="text-body1">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas,
+      architecto. Cum, amet placeat a molestiae expedita voluptas similique
+      totam sit doloremque libero dignissimos, dolores accusamus illo
+      asperiores, vitae ipsa eveniet! Id aspernatur ipsum molestias in sequi
+      nostrum quis delectus perspiciatis iste quibusdam, harum temporibus omnis
+      tenetur sit eum illum labore, voluptatum aliquid porro voluptas quo
+      accusamus ipsa eaque? Hic, culpa! Ducimus, est! Labore perferendis nihil
+      neque numquam necessitatibus animi ut ratione tempore autem, quasi
+      tenetur?
+    </p>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageAbout'
-}
+  name: "PageAbout",
+};
 </script>
