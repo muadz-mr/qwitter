c824b737381e1a40c28f127ac0b7c475006f5bd4
Mu'adz Bin Mohd Rosli <muadz.mrosli@gmail.com>
2023-10-09 16:25:14 +0800

    feat: add qweets list ui on home page

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -13,7 +13,7 @@
         >
           <template v-slot:before>
             <q-avatar size="xl">
-              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+              <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
             </q-avatar>
           </template>
         </q-input>
@@ -30,7 +30,42 @@
         />
       </div>
     </div>
+
     <q-separator class="divider" size="10px" color="grey-2"></q-separator>
+
+    <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <strong>John Doe</strong>
+            <span class="q-ml-sm text-grey-7">@john_doe</span>
+          </q-item-label>
+          <q-item-label class="qweet-content text-body1"
+            >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
+            provident labore assumenda voluptatibus expedita? Deleniti unde quos
+            saepe voluptates totam dolore asperiores earum praesentium ipsum cum
+            ad numquam magnam sequi cumque, ut, debitis laborum, distinctio
+            doloribus optio molestiae quaerat tempora. Non iusto nemo natus
+            recusandae quia necessitatibus illo voluptas
+            consequuntur.</q-item-label
+          >
+          <div class="row justify-between q-mt-sm">
+            <q-btn flat round size="sm" color="grey" icon="fa-regular fa-comment" />
+            <q-btn flat round size="sm" color="grey" icon="fa-solid fa-retweet" />
+            <q-btn flat round size="sm" color="grey" icon="fa-regular fa-heart" />
+            <q-btn flat round size="sm" color="grey" icon="fa-regular fa-trash-can" />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top> 1 min ago </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -50,4 +85,9 @@ export default {
   border-top: 1px solid;
   border-bottom: 1px solid;
   border-color: $grey-4;
-}</style>
+}
+
+.qweet-content {
+  white-space: pre-line;
+}
+</style>
